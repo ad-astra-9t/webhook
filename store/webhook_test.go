85e9a2bbf0a1882874c5413b20c00e328126f3c4
Dean Lin <richer6605@gmail.com>
2022-11-17 21:35:56 +0800

    Add test name for webhook store test

--- store/webhook_test.go
+++ store/webhook_test.go
@@ -9,23 +9,25 @@ import (
 )
 
 func TestCreateWebhook(t *testing.T) {
-	store := WebhookStore{
-		db: db.MustNewDB(
-			"postgres",
-			"host=localhost port=5431 user=test password=test dbname=testdb sslmode=disable",
-		),
-		tableName: "webhooks",
-		dbAdapter: db.AdaptWebhook,
-	}
-	w1 := domain.Webhook{Callback: "https://callback.com"}
+	t.Run("Test create webhook", func(t *testing.T) {
+		store := WebhookStore{
+			db: db.MustNewDB(
+				"postgres",
+				"host=localhost port=5431 user=test password=test dbname=testdb sslmode=disable",
+			),
+			tableName: "webhooks",
+			dbAdapter: db.AdaptWebhook,
+		}
+		w1 := domain.Webhook{Callback: "https://callback.com"}
 
-	err := store.CreateWebhook(w1)
-	if err != nil {
-		t.Fatalf("Failed to create webhook: %s\n", err.Error())
-	}
+		err := store.CreateWebhook(w1)
+		if err != nil {
+			t.Fatalf("Failed to create webhook: %s\n", err.Error())
+		}
 
-	w2, err := store.GetWebhook(w1)
-	if err != nil || !reflect.DeepEqual(w2, w1) {
-		t.Errorf("Webhook is created incorrectly, err: %#v, got: %#v, want: %#v\n", err, w2, w1)
-	}
+		w2, err := store.GetWebhook(w1)
+		if err != nil || !reflect.DeepEqual(w2, w1) {
+			t.Errorf("Webhook is created incorrectly, err: %#v, got: %#v, want: %#v\n", err, w2, w1)
+		}
+	})
 }
