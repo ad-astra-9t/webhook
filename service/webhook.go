a13dc7f03673f0bc26e9744b223a90e272cf978a
Dean Lin <richer6605@gmail.com>
2022-11-17 22:00:08 +0800

    Add webhook service

---
+++ service/webhook.go
@@ -0,0 +1,20 @@
+package service
+
+import "github.com/ad-astra-9t/webhook/domain"
+
+type WebhookStore interface {
+	CreateWebhook(domain.Webhook) error
+	GetWebhook(domain.Webhook) (domain.Webhook, error)
+}
+
+type WebhookService struct {
+	store WebhookStore
+}
+
+func (s *WebhookService) CreateWebhook(webhook domain.Webhook) error {
+	return s.store.CreateWebhook(webhook)
+}
+
+func (s *WebhookService) GetWebhook(webhook domain.Webhook) error {
+	return s.store.CreateWebhook(webhook)
+}
