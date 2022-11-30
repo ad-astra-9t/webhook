09a4abfa2321bb9fbf6079f0f07a328cef3f9d94
Dean Lin <richer6605@gmail.com>
2022-11-30 23:54:18 +0800

    Add `EventModel` to `Model`

--- model/model.go
+++ model/model.go
@@ -4,10 +4,12 @@ import "github.com/ad-astra-9t/webhook/dbx"
 
 type Model struct {
 	WebhookModel
+	EventModel
 }
 
 func NewModel(dbx *dbx.DBX) *Model {
 	return &Model{
 		NewWebhookModel(dbx),
+		NewEventModel(dbx),
 	}
 }
