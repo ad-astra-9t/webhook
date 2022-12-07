debf68e5cd4b709b3e19eb86db71575584ccd961
Dean Lin <richer6605@gmail.com>
2022-12-08 00:05:55 +0800

    Rename `AutoTxModel` interface to `TxModel`
    `AutoTxModel` should be an interface that describes automatic transaction behavior in store package.
    `TxModel` is an interface that describes common behavior of a transaction or a model.

--- model/modelx.go
+++ model/modelx.go
@@ -7,19 +7,19 @@ type Modelx struct {
 	Tx *ModelTx
 }
 
-type AutoTxModel interface {
+type TxModel interface {
 	GetWebhook(target Webhook) (result Webhook, err error)
 	CreateWebhook(target Webhook) error
 }
 
-func (m *Modelx) AutoTxModel() (AutoTxModel, error) {
+func (m *Modelx) AutoTx() (TxModel, error) {
 	if m.Tx != nil {
 		return m.Tx, nil
 	}
 	if m.Model != nil {
 		return m.Model, nil
 	}
-	return nil, errors.New("failed to convert Modelx to AutoTxModel")
+	return nil, errors.New("failed to convert Modelx to TxModel")
 }
 
 func NewModelx(model *Model) *Modelx {
