236ced88208760f5c17ae784cba03eca41a427b7
Dean Lin <richer6605@gmail.com>
2022-12-09 22:46:41 +0800

    Use automatic transaction in store

--- store/webhook.go
+++ store/webhook.go
@@ -11,13 +11,24 @@ type WebhookStore struct {
 
 func (s WebhookStore) CreateWebhook(target domain.Webhook) error {
 	modeltarget := s.modelx.AdaptModel(target)
-	return s.modelx.CreateWebhook(modeltarget)
+
+	txmodel, err := s.modelx.AutoTx()
+	if err != nil {
+		return err
+	}
+
+	return txmodel.CreateWebhook(modeltarget)
 }
 
 func (s WebhookStore) GetWebhook(target domain.Webhook) (result domain.Webhook, err error) {
 	modeltarget := s.modelx.AdaptModel(target)
 
-	modelresult, err := s.modelx.GetWebhook(modeltarget)
+	txmodel, err := s.modelx.AutoTx()
+	if err != nil {
+		return result, err
+	}
+
+	modelresult, err := txmodel.GetWebhook(modeltarget)
 	if err != nil {
 		return result, err
 	}
