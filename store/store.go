97328d615ba084faab69da1f8f2bd1ba20e4734f
Dean Lin <richer6605@gmail.com>
2022-12-09 22:50:40 +0800

    Add event store to store

--- store/store.go
+++ store/store.go
@@ -4,10 +4,12 @@ import "github.com/ad-astra-9t/webhook/modelx"
 
 type Store struct {
 	WebhookStore
+	EventStore
 }
 
 func NewStore(modelx *modelx.Modelx) *Store {
 	return &Store{
-		WebhookStore: NewWebhookStore(modelx),
+		NewWebhookStore(modelx),
+		NewEventStore(modelx),
 	}
 }
