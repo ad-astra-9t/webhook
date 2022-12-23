61132066a8d898e4af9685c50185d68a177f6948
Dean Lin <richer6605@gmail.com>
2022-12-24 00:23:11 +0800

    Move model transaction structure from `model.go` to `modelx.go`

--- modelx/model.go
+++ modelx/model.go
@@ -1,55 +1,6 @@
 package modelx
 
-import (
-	"context"
-
-	"github.com/ad-astra-9t/webhook/tx"
-)
-
 type Model interface {
 	GetWebhook(target Webhook) (result Webhook, err error)
 	CreateWebhook(target Webhook) error
-}
-
-type DBXModel struct {
-	dbx *tx.DBX
-	WebhookModel
-	EventModel
-}
-
-type ModelTx struct {
-	*DBXModel
-}
-
-func (m *DBXModel) Tx(ctx context.Context) (*ModelTx, error) {
-	dbxCopy := new(tx.DBX)
-	*dbxCopy = *m.dbx
-
-	if err := dbxCopy.SetTx(ctx); err != nil {
-		return nil, err
-	}
-
-	modelCopy := &DBXModel{
-		dbxCopy,
-		NewWebhookModel(dbxCopy),
-		NewEventModel(dbxCopy),
-	}
-
-	return &ModelTx{modelCopy}, nil
-}
-
-func (m *ModelTx) Cancel() error {
-	return m.dbx.Tx.Rollback()
-}
-
-func (m *ModelTx) End() error {
-	return m.dbx.Tx.Commit()
-}
-
-func NewDBXModel(dbx *tx.DBX) *DBXModel {
-	return &DBXModel{
-		dbx,
-		NewWebhookModel(dbx),
-		NewEventModel(dbx),
-	}
 }
--- modelx/modelx.go
+++ modelx/modelx.go
@@ -2,11 +2,23 @@ package modelx
 
 import (
 	"context"
+
+	"github.com/ad-astra-9t/webhook/tx"
 )
 
 type Modelx struct {
 	*DBXModel
 	Tx *ModelTx
+}
+
+type DBXModel struct {
+	dbx *tx.DBX
+	WebhookModel
+	EventModel
+}
+
+type ModelTx struct {
+	*DBXModel
 }
 
 func (m *Modelx) SetTx(ctx context.Context) error {
@@ -18,6 +30,39 @@ func (m *Modelx) SetTx(ctx context.Context) error {
 	m.Tx = tx
 
 	return nil
+}
+
+func (m *DBXModel) Tx(ctx context.Context) (*ModelTx, error) {
+	dbxCopy := new(tx.DBX)
+	*dbxCopy = *m.dbx
+
+	if err := dbxCopy.SetTx(ctx); err != nil {
+		return nil, err
+	}
+
+	modelCopy := &DBXModel{
+		dbxCopy,
+		NewWebhookModel(dbxCopy),
+		NewEventModel(dbxCopy),
+	}
+
+	return &ModelTx{modelCopy}, nil
+}
+
+func (m *ModelTx) Cancel() error {
+	return m.dbx.Tx.Rollback()
+}
+
+func (m *ModelTx) End() error {
+	return m.dbx.Tx.Commit()
+}
+
+func NewDBXModel(dbx *tx.DBX) *DBXModel {
+	return &DBXModel{
+		dbx,
+		NewWebhookModel(dbx),
+		NewEventModel(dbx),
+	}
 }
 
 func NewModelx(model *DBXModel) *Modelx {
