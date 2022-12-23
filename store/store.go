4d07cb8f33e41fd9a7a50ee2509cc9092f3e0630
Dean Lin <richer6605@gmail.com>
2022-12-24 01:20:14 +0800

    Add `DefaultStore`
    `DefaultStore` is an implementation of `Store` that uses `DefaultModel`

--- store/store.go
+++ store/store.go
@@ -2,9 +2,24 @@ package store
 
 import (
 	"github.com/ad-astra-9t/webhook/domain"
+	"github.com/ad-astra-9t/webhook/model"
 )
 
 type Store interface {
 	CreateWebhook(target domain.Webhook) error
 	GetWebhook(target domain.Webhook) (result domain.Webhook, err error)
+}
+
+type DefaultStore struct {
+	*model.DefaultModel
+	WebhookStore
+	EventStore
+}
+
+func NewDefaultStore(model *model.DefaultModel, adapt *model.ModelAdapt) *DefaultStore {
+	return &DefaultStore{
+		model,
+		NewWebhookStore(model, adapt),
+		NewEventStore(model),
+	}
 }
