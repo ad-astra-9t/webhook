649cbf6f39a7bc2edff0963fbe88df481af228dd
Dean Lin <richer6605@gmail.com>
2022-12-24 01:16:16 +0800

    Move store transaction structure from `store.go` to `storex.go`

--- store/store.go
+++ store/store.go
@@ -1,60 +1,10 @@
 package store
 
 import (
-	"context"
-
 	"github.com/ad-astra-9t/webhook/domain"
-	"github.com/ad-astra-9t/webhook/model"
-	"github.com/ad-astra-9t/webhook/tx"
 )
 
 type Store interface {
 	CreateWebhook(target domain.Webhook) error
 	GetWebhook(target domain.Webhook) (result domain.Webhook, err error)
-}
-
-type ModelxStore struct {
-	modelx *tx.Modelx
-	adapt  *model.ModelAdapt
-	WebhookStore
-	EventStore
-}
-
-type StoreTx struct {
-	*ModelxStore
-}
-
-func (s *ModelxStore) Tx(ctx context.Context) (*StoreTx, error) {
-	modelxCopy := new(tx.Modelx)
-	*modelxCopy = *s.modelx
-
-	if err := modelxCopy.SetTx(ctx); err != nil {
-		return nil, err
-	}
-
-	storeCopy := &ModelxStore{
-		modelxCopy,
-		s.adapt,
-		NewWebhookStore(modelxCopy, s.adapt),
-		NewEventStore(modelxCopy),
-	}
-
-	return &StoreTx{storeCopy}, nil
-}
-
-func (s *StoreTx) Cancel() error {
-	return s.modelx.Tx.Cancel()
-}
-
-func (s *StoreTx) End() error {
-	return s.modelx.Tx.End()
-}
-
-func NewStore(modelx *tx.Modelx, adapt *model.ModelAdapt) *ModelxStore {
-	return &ModelxStore{
-		modelx,
-		adapt,
-		NewWebhookStore(modelx, adapt),
-		NewEventStore(modelx),
-	}
 }
