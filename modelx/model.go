e465f251f2a8adcaba78543af77127747168ccdf
Dean Lin <richer6605@gmail.com>
2022-12-24 00:38:30 +0800

    Add `DefaultModel`
    `DefaultModel` is an implementation of `Model` that uses `sqlx.DB`

--- modelx/model.go
+++ modelx/model.go
@@ -1,6 +1,22 @@
 package modelx
 
+import "github.com/jmoiron/sqlx"
+
 type Model interface {
 	GetWebhook(target Webhook) (result Webhook, err error)
 	CreateWebhook(target Webhook) error
+}
+
+type DefaultModel struct {
+	*sqlx.DB
+	WebhookModel
+	EventModel
+}
+
+func NewDefaultModel(db *sqlx.DB) *DefaultModel {
+	return &DefaultModel{
+		db,
+		NewWebhookModel(db),
+		NewEventModel(db),
+	}
 }
