2aa6e08a0497779818027454d3e67a5ce0cb50c7
Dean Lin <richer6605@gmail.com>
2023-01-02 00:27:51 +0800

    Test cancel transaction when creating webhooks

--- tx/storex_test.go
+++ tx/storex_test.go
@@ -1,6 +1,8 @@
 package tx
 
 import (
+	"context"
+	"database/sql"
 	"reflect"
 	"testing"
 
@@ -32,6 +34,45 @@ func TestStorex_CreateWebhook(t *testing.T) {
 		result, err := storex.GetWebhook(target)
 		if err != nil || !reflect.DeepEqual(result, target) {
 			t.Errorf("Webhook is created incorrectly, err: %#v, got: %#v, want: %#v\n", err, result, target)
+		}
+	})
+
+	t.Run("Test cancel transaction when creating webhooks", func(t *testing.T) {
+		ctx := context.Background()
+
+		err := storex.SetTx(ctx)
+		if err != nil {
+			t.Fatalf("Store failed to start transaction. err: %s\n", err.Error())
+		}
+
+		targets := []domain.Webhook{
+			{Callback: "https://callback1.com"},
+			{Callback: "https://callback2.com"},
+		}
+		for _, target := range targets {
+			err := storex.CreateWebhook(target)
+			if err != nil {
+				t.Fatalf("Store failed to create webhook. err: %s, target: %#v\n", err.Error(), target)
+			}
+			result, err := storex.GetWebhook(target)
+			if err != nil {
+				t.Fatalf("Store failed to get webhook. err: %s, target: %#v\n", err.Error(), target)
+			}
+			if target != result {
+				t.Fatalf("Webhook is incorrectly created. got: %#v, want: %#v\n", result, target)
+			}
+		}
+
+		err = storex.Cancel()
+		if err != nil {
+			t.Fatalf("Store failed to cancel transaction. err: %s\n", err.Error())
+		}
+		result, err := storex.GetWebhook(targets[0])
+		if err == nil {
+			t.Fatalf("Transaction is incorrectly cancelled. result: %#v\n", result)
+		}
+		if err != sql.ErrNoRows {
+			t.Fatalf("Store failed to get webhook. err: %s, target: %#v\n", err.Error(), targets[0])
 		}
 	})
 }
