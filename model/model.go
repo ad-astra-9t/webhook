d0d2173129cc768e9bbdf625a3222371fa72273b
Dean Lin <richer6605@gmail.com>
2023-01-09 00:25:58 +0800

    Refactor transaction functionality and remove `etx` package

--- model/model.go
+++ model/model.go
@@ -1,6 +1,11 @@
 package model
 
-import "github.com/jmoiron/sqlx"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type Model interface {
 	GetWebhook(target Webhook) (result Webhook, err error)
@@ -8,9 +13,36 @@ type Model interface {
 }
 
 type DefaultModel struct {
-	*sqlx.DB
+	db *sqlx.DB
 	WebhookModel
 	EventModel
+}
+
+func (m *DefaultModel) Tx(ctx context.Context, options *sql.TxOptions) (*TxModel, error) {
+	tx, err := m.db.BeginTxx(ctx, options)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TxModel{
+		tx,
+		NewWebhookModel(tx),
+		NewEventModel(tx),
+	}, nil
+}
+
+type TxModel struct {
+	tx *sqlx.Tx
+	WebhookModel
+	EventModel
+}
+
+func (m *TxModel) Cancel() error {
+	return m.tx.Rollback()
+}
+
+func (m *TxModel) End() error {
+	return m.tx.Commit()
 }
 
 func NewDefaultModel(db *sqlx.DB) *DefaultModel {
--- store/store.go
+++ store/store.go
@@ -1,6 +1,9 @@
 package store
 
 import (
+	"context"
+	"database/sql"
+
 	"github.com/ad-astra-9t/webhook/domain"
 	"github.com/ad-astra-9t/webhook/model"
 )
@@ -11,14 +14,45 @@ type Store interface {
 }
 
 type DefaultStore struct {
-	*model.DefaultModel
+	defaultModel *model.DefaultModel
+	modelAdapt   *model.ModelAdapt
 	WebhookStore
 	EventStore
+}
+
+func (s *DefaultStore) Tx(ctx context.Context, options *sql.TxOptions) (*TxStore, error) {
+	tx, err := s.defaultModel.Tx(ctx, options)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TxStore{
+		tx,
+		s.modelAdapt,
+		NewWebhookStore(tx, s.modelAdapt),
+		NewEventStore(tx),
+	}, nil
+}
+
+type TxStore struct {
+	txModel    *model.TxModel
+	modelAdapt *model.ModelAdapt
+	WebhookStore
+	EventStore
+}
+
+func (m *TxStore) Cancel() error {
+	return m.txModel.Cancel()
+}
+
+func (m *TxStore) End() error {
+	return m.txModel.End()
 }
 
 func NewDefaultStore(model *model.DefaultModel, adapt *model.ModelAdapt) *DefaultStore {
 	return &DefaultStore{
 		model,
+		adapt,
 		NewWebhookStore(model, adapt),
 		NewEventStore(model),
 	}
